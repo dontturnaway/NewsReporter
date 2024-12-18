2de862273df1c4fb4813b8f6ed1534f7b0cc8cb2
dlabyshkin <dlabyshkin@eyeconweb.com>
2024-12-18 17:29:06 +0200

    Added all newscases

--- src/main/java/com/decode/newsreporter/application/usecase/get_news_report/GetNewsReportResponse.java
+++ src/main/java/com/decode/newsreporter/application/usecase/get_news_report/GetNewsReportResponse.java
@@ -1,4 +1,4 @@
 package com.decode.newsreporter.application.usecase.get_news_report;
 
-public record GetNewsReportResponse(String htmlReport) {
+public record GetNewsReportResponse(String url) {
 }
