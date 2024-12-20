3e1f25519a95fc72562c569767fffb558f357969
dlabyshkin <dlabyshkin@eyeconweb.com>
2024-12-20 21:21:08 +0200

    Refactored DTO and Controller

--- src/main/java/com/decode/newsreporter/infrastructure/repository/NewsRepositoryORM.java
+++ src/main/java/com/decode/newsreporter/infrastructure/repository/NewsRepositoryORM.java
@@ -1,6 +1,5 @@
 package com.decode.newsreporter.infrastructure.repository;
 
-
 import com.decode.newsreporter.infrastructure.entity.NewsORM;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
