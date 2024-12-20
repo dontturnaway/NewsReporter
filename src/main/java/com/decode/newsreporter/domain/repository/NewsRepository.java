db0593470ab253a0b88896e3a6ff519fbfa41ab3
dlabyshkin <dlabyshkin@eyeconweb.com>
2024-12-20 21:27:16 +0200

    Renamed Service

--- src/main/java/com/decode/newsreporter/domain/repository/NewsRepository.java
+++ src/main/java/com/decode/newsreporter/domain/repository/NewsRepository.java
@@ -1,13 +1,12 @@
 package com.decode.newsreporter.domain.repository;
 
-import com.decode.newsreporter.infrastructure.controller.WrongNewsIdProvided;
 import com.decode.newsreporter.domain.entity.NewsDTO;
 
 import java.util.List;
 
 public interface NewsRepository {
     List<NewsDTO> getAllNews();
-    NewsDTO getNewsById(Long id) throws WrongNewsIdProvided;
-    List<NewsDTO> getNewsById(List<Long> ids) throws WrongNewsIdProvided;
+    NewsDTO getNewsById(Long id);
+    List<NewsDTO> getNewsById(List<Long> ids);
     NewsDTO save(NewsDTO news) ;
 }
